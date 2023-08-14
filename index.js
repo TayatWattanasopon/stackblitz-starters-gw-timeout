15a923ad2a89bffaf19137ed446e525052c96cbd
TayatWattanasopon <113895105+TayatWattanasopon@users.noreply.github.com>
2023-08-14 11:38:45 +0700

    3 files modified

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require('express');
+const app = express();
+
+app.get('/simulate-timeout', (req, res) => {
+  // Introduce a delay before responding to simulate a timeout
+  setTimeout(() => {
+    res.status(503).send('Gateway Timeout');
+  }, 10000); // Simulating a 10-second delay
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
